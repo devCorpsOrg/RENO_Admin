4f230548d408bb0d7b4e1332fe048f4113970e5f
adarshkumar5776 <85995654+adarshkumar5776@users.noreply.github.com>
2023-04-07 01:38:33 +0530

    Update models.py

--- renoadminbackend/reno/renoadmin_apis/models.py
+++ renoadminbackend/reno/renoadmin_apis/models.py
@@ -25,12 +25,13 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
 
 
 class Userdetails(models.Model):
+    id=models.IntegerField(primary_key=True)
     username=models.CharField(max_length=100)
     about=models.TextField()
     email=models.EmailField(max_length=256)
     phone=models.CharField(max_length=100)
     role=models.CharField(max_length=100)
-    uid=models.CharField(max_length=100)
+    uid=models.IntegerField(unique=True)
     pic=models.ImageField(upload_to="my_pic",blank=True)
     is_suspend=models.BooleanField(default=False)
     suspend_reason=models.TextField(null=True,blank=True)
@@ -54,14 +55,15 @@ class User_Product(models.Model):
     p_url=models.CharField(max_length=100)
     
 class cmsModel(models.Model): 
+    id=models.IntegerField(primary_key=True)
     pagename=models.CharField(max_length=100)
-    pageid=models.IntegerField()
+    pageid=models.IntegerField(unique=True)
     title=models.CharField(max_length=100)
     content=models.CharField(max_length=100)    
     media=models.ImageField(upload_to="my_pic",blank=True)
 
 class ProjectManagementModel(models.Model):
-    
+    id=models.IntegerField(primary_key=True)
     pic=models.ImageField(upload_to="my_pic",blank=True)
     proj_name=models.CharField(max_length=200)
     proj_category=models.CharField(max_length=500)
@@ -69,10 +71,10 @@ class ProjectManagementModel(models.Model):
     review=models.TextField(max_length=100)
     details=models.TextField()       
     project_type=models.CharField(max_length=100)       
-    proj_id=models.IntegerField()
+    proj_id=models.IntegerField(unique=True)
 
 class Projectbooking(models.Model):
-    
+    id=models.IntegerField(primary_key=True)
     date = models.DateField(default=datetime.date.today)
     time=models.TimeField(auto_now_add=True, blank=True)
     status=models.CharField(max_length=500)
@@ -83,19 +85,20 @@ class Projectbooking(models.Model):
     desc=models.TextField()
     
 class pmsModel(models.Model):
-    
+    id=models.IntegerField(primary_key=True)
     pic=models.ImageField(upload_to="my_pic",blank=True)
     prod_name=models.CharField(max_length=200)
     prod_category=models.CharField(max_length=100)
     inv_count=models.IntegerField()
     rate=models.IntegerField()        
-    prod_id=models.IntegerField()
+    prod_id=models.IntegerField(unique=True)
 
 class SupportDetails(models.Model): 
+    id=models.IntegerField(primary_key=True)
     pic=models.ImageField(upload_to="my_pic",blank=True)   
-    member_name=models.CharField(max_length=100)
-    phone=models.CharField(max_length=100)
-    note=models.TextField()
-    inv_count=models.IntegerField()
-    c_id=models.CharField(max_length=100)
+    registerar=models.CharField(max_length=100)
+    subject=models.CharField(max_length=100)
+    message=models.TextField()
+    status=models.CharField(max_length=100)
+    support_id=models.IntegerField(unique=True)
     
