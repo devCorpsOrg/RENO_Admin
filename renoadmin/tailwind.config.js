e8f041d066b910935a81e924e4222629c5efe44f
Prashant142 <prashantdhiman142@gmail.com>
2023-04-11 17:36:10 +0530

    Updated the tailwind.config.js

--- renoadmin/tailwind.config.js
+++ renoadmin/tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -7,16 +6,3 @@ module.exports = {
   },
   plugins: [],
 };
-=======
-
-module.exports = {
-  content: ['./src/**/*.{html,js,jsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("tailwind-scrollbar-hide"),
-  ],
-}
-
->>>>>>> main
