3565e0f54890942a64b1321077847f46725195ca
KoushikAD1234 <koushikjio1234@gmail.com>
2023-04-14 12:35:34 +0530

    Changes

--- renoadmin/src/Pages/MarketPlaceManagement/ListingManagement/editListing.jsx
+++ renoadmin/src/Pages/MarketPlaceManagement/ListingManagement/editListing.jsx
@@ -1,1 +1,203 @@
-// Edit list
+import { useState } from "react";
+import React from "react";
+import TopHeader from "../../../UI/TopHeader/TopHeader";
+
+const EditListing = ({ setExpand, setActiveTab }) => {
+  setExpand("marketPlace");
+  setActiveTab("listingManagement");
+  const head = "Edit Listing";
+
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [content, setContent] = useState("");
+  const [images, setImages] = useState([]);
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(title, price); // Do something with the data
+    setTitle("");
+    setPrice("");
+    setImages([]);
+  };
+
+  const handlePhotoUpload = (event) => {
+    const files = event.target.files;
+    const uploadedImages = [];
+    for (let i = 0; i < files.length; i++) {
+      uploadedImages.push({
+        name: files[i].name,
+        url: URL.createObjectURL(files[i]),
+      });
+    }
+    setImages(uploadedImages);
+  };
+
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+
+  return (
+    <div>
+      <div className="flex fixed z-10">
+        <TopHeader className="fixed" head={head} />
+      </div>
+
+      <div className=" ml-72 mb-10 relative" style={{ marginTop: "70px" }}>
+        <form onSubmit={handleSubmit}>
+          <div style={{ marginRight: 0, marginLeft: 920 }}>
+            <button
+              className="rounded mt-10"
+              style={{
+                backgroundColor: "black",
+                width: "130px",
+                height: "47px",
+                color: "white",
+              }}
+              type="submit">
+              Cancel
+            </button>
+
+            <button
+              className="rounded mt-10"
+              style={{
+                backgroundColor: "rgba(153, 190, 17, 0.831)",
+                width: "130px",
+                height: "47px",
+                color: "white",
+                marginLeft: "30px",
+              }}
+              type="submit">
+              Save
+            </button>
+          </div>
+          <label className="grid mt-5">
+            Service Name
+            <input
+              type="text"
+              placeholder="Electician Services"
+              id="title"
+              className="rounded outline-none"
+              style={{
+                height: "50px",
+                width: "1210px",
+                paddingLeft: "10px",
+                backgroundColor: "#e5ecff",
+                marginTop: "5px",
+                fontSize: "15px",
+              }}
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+            />
+          </label>
+
+          <div className="grid grid-cols-2 gap-3 mt-5">
+            <label className="grid pr-6">
+              Price
+              <input
+                id="label"
+                class="outline-none rounded"
+                placeholder="$000.00"
+                style={{
+                  height: "50px",
+                  width: "590px",
+                  paddingLeft: "5px",
+                  backgroundColor: "#e5ecff",
+                  marginTop: "5px",
+                  fontSize: "14px",
+                }}
+                value={price}
+                onChange={handlePriceChange}
+              />
+            </label>
+            <label className="grid pr-6">
+              Service Photos
+              <input
+                style={{
+                  height: "50px",
+                  width: "590px",
+                  paddingLeft: "0px",
+                  border: "2px solid 	#e6f7fe",
+                  marginTop: "5px",
+                  fontSize: "14px",
+                }}
+                class="file:bg-black file:px-6 file:py-3 file:border-none file:rounded file:text-white file:cursor-pointer placeholder-transparent mt-3 rounded appearance-none placeholder-transparent"
+                type="file"
+                accept="image/*"
+                multiple
+                onChange={handlePhotoUpload}
+                placeholder=""
+              />
+            </label>
+            <div style={{ marginLeft: "625px", width: "600px" }}>
+              {images && images.length > 0 && (
+                <div className="grid grid-cols-4 gap-3">
+                  {images.map((image, index) => (
+                    <img
+                      key={index}
+                      src={image.url} // replace with your image source
+                      alt={image.name} // replace with your image alt text
+                      style={{
+                        width: "100px",
+                        height: "100px",
+                        objectFit: "cover",
+                        marginRight: "10px",
+                      }} // set width, height, object-fit, and margin-right styles
+                    />
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+
+          <label className="grid mt-5">
+            Description
+            <textarea
+              id="content"
+              placeholder="Enter Description"
+              className="rounded outline-none pt-2"
+              style={{
+                height: "170px",
+                width: "1210px",
+                backgroundColor: "#e5ecff",
+                paddingLeft: "10px",
+                paddingTop: "20px",
+                fontSize: "15px",
+                marginTop: "5px",
+              }}
+              value={content}
+              onChange={(event) => setContent(event.target.value)}
+            />
+          </label>
+          {/* <div> */}
+          <button
+            className="rounded mt-10"
+            style={{
+              backgroundColor: "rgba(153, 190, 17, 0.831)",
+              width: "170px",
+              height: "55px",
+              color: "white",
+            }}
+            type="submit">
+            Save
+          </button>
+          <button
+            className="rounded mt-10"
+            style={{
+              backgroundColor: "black",
+              width: "170px",
+              height: "55px",
+              color: "white",
+              marginLeft: "30px",
+            }}
+            type="submit">
+            Cancel
+          </button>
+          {/* </div> */}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditListing;
