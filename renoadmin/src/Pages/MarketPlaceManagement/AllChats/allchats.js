bd10a761312c1669d028ce9583306235c6ee048f
Prashant142 <prashantdhiman142@gmail.com>
2023-04-14 15:51:40 +0530

    Added new chat management and help desk tables

---
+++ renoadmin/src/Pages/MarketPlaceManagement/AllChats/allchats.js
@@ -0,0 +1,112 @@
+import React from "react";
+import Table from "../../../UI/CommonTable/Table";
+import { deleteIcon, edit, images } from "../Assets/index";
+import TopHeader from "../../../UI/TopHeader/TopHeader";
+import { useNavigate } from "react-router-dom";
+import Status from "./Status";
+
+const Action = () => {
+  const Navigate = useNavigate();
+  const handleClick = () => {
+    Navigate("/home/editMember");
+  };
+  return (
+    <div className="w-6 h-6 flex gap-3 cursor-pointer">
+      <img onClick={handleClick} src={edit} alt="edit" />
+      <img src={deleteIcon} alt="Delete" />
+    </div>
+  );
+};
+
+const Photo = () => {
+  return (
+    <div>
+      <img className="w-14 h-14 rounded" src={images} alt="Photo" />
+    </div>
+  );
+};
+
+const columns = [
+  {
+    header: "Photo",
+    accessor: "photo",
+  },
+  {
+    header: "Requester",
+    accessor: "requester",
+  },
+  {
+    header: "Subject",
+    accessor: "subject",
+  },
+  {
+    header: "Message",
+    accessor: "message",
+  },
+  {
+    header: "Status",
+    accessor: "status",
+  },
+  {
+    header: "Action",
+    accessor: "action",
+  },
+];
+
+const data = [
+  {
+    photo: <Photo />,
+    requester: "Anges",
+    subject: "Delivery Status",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    status: <Status value={"Solved"} />,
+    action: <Action />,
+  },
+  {
+    photo: <Photo />,
+    requester: "Anges",
+    subject: "Delivery Status",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    status: <Status value={"Open"} />,
+    action: <Action />,
+  },
+  {
+    photo: <Photo />,
+    requester: "Anges",
+    subject: "Delivery Status",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    status: <Status value={"Pending"} />,
+    action: <Action />,
+  },
+  {
+    photo: <Photo />,
+    requester: "Anges",
+    subject: "Delivery Status",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    status: <Status value={"Pending"} />,
+    action: <Action />,
+  },
+];
+
+// Number of Pages to be display on a single page.
+const pageSize = 4;
+
+const AllProjects = () => {
+  const head = "All Chats and Deals";
+  return (
+    <div>
+      <div className="flex fixed z-10">
+        <TopHeader className="fixed" head={head} />
+      </div>
+      <div className=" ml-72 mt-28 h-[85vh] min-w-[80%] relative">
+        <Table columns={columns} data={data} pageSize={pageSize} />
+      </div>
+    </div>
+  );
+};
+
+export default AllProjects;
