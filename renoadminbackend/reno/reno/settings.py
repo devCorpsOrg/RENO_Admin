963c353e10869c5fbdb2b3f5b1e3cdf1de177f6b
adarshkumar5776 <adarshyadav.5776@gmail.com>
2023-04-18 17:12:56 +0530

    update

--- renoadminbackend/reno/reno/settings.py
+++ renoadminbackend/reno/reno/settings.py
@@ -150,3 +150,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
